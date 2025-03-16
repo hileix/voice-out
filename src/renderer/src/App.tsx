80eb87ef542f350c43cd47217a3ac8f20683f7e4
hileix <hileix@outlook.com>
2025-03-16 23:22:08 +0800

    fix

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import Settings from './components/Settings'
-import electronLogo from './assets/electron.svg'
 import './App.css'
 
 // Define the shortcut type
